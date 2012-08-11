13af302a18619882f5220a1368330022c7380a6f
Morgante Pell <morgante.pell@morgante.net>
2012-08-10 22:31:17 -0400

    manually copy over miklb/habari-jambo-formui

--- jambo.plugin.php
+++ jambo.plugin.php
@@ -8,45 +8,108 @@
  * @todo document the functions.
  * @todo use AJAX to submit form, fallback on default if no AJAX.
  * @todo allow "custom fields" to be added by user.
+ * @todo redo the hook and make it easy to add other formui comment stuff.
+ * @todo use Habari's spam filtering.
  */
 
-require_once 'jambohandler.php';
-
-class Jambo extends Plugin
-{
-	const VERSION = '1.4.2';
-	const OPTION_NAME = 'jambo';
-	
-	private $theme;
-	
-	public function help()
-	{
-		return _t( 'Add a contact form by inserting either <code>&lt;!-- jambo --&gt;</code> or <code>&lt;!-- contactform --&gt;</code> into an entry or page.' );
-	}
-	
-	private static function default_options()
-	{
-		return array(
-			'send_to' => $_SERVER['SERVER_ADMIN'],
-			'subject_prefix' => _t( '[CONTACT FORM] ' ),
-			'show_form_on_success' => 1,
-			'success_msg' => _t( 'Thank you for your feedback. I\'ll get back to you as soon as possible.' ),
-			'error_msg' => _t( 'The following errors occurred with the information you submitted. Please correct them and re-submit the form.' )
-			);
-	}
-	
-	public function action_plugin_activation( $file )
-	{
-		if ( Plugins::id_from_file( $file ) == Plugins::id_from_file( __FILE__ ) ) {
-			foreach ( self::default_options() as $name => $value ) {
-				Options::set( self::OPTION_NAME . '__' . $name, $value );
-			}
+class JamboFormUI extends Plugin
+{	
+	public function get_jambo_form( ) {
+		// borrow default values from the comment forms
+		$commenter_name = '';
+		$commenter_email = '';
+		$commenter_url = '';
+		$commenter_content = '';
+		$user = User::identify();
+		if ( isset( $_SESSION['comment'] ) ) {
+			$details = Session::get_set( 'comment' );
+			$commenter_name = $details['name'];
+			$commenter_email = $details['email'];
+			$commenter_url = $details['url'];
+			$commenter_content = $details['content'];
 		}
+		elseif ( $user->loggedin ) {
+			$commenter_name = $user->displayname;
+			$commenter_email = $user->email;
+			$commenter_url = Site::get_url( 'habari' );
+		}
+
+		// Now start the form.
+		$form = new FormUI( 'jambo' );
+// 		$form->set_option( 'form_action', URL::get( 'submit_feedback', array( 'id' => $this->id ) ) );
+
+		// Create the Name field
+		$form->append(
+			'text',
+			'jambo_name',
+			'null:null',
+			_t( 'Name' ),
+			'formcontrol_text'
+		)->add_validator( 'validate_required', _t( 'Your Name is required.' ) )
+		->id = 'jambo_name';
+		$form->jambo_name->tabindex = 1;
+		$form->jambo_name->value = $commenter_name;
+
+		// Create the Email field
+		$form->append(
+			'text',
+			'jambo_email',
+			'null:null',
+			_t( 'Email' ),
+			'formcontrol_text'
+		)->add_validator( 'validate_email', _t( 'Your Email must be a valid address.' ) )
+		->id = 'jambo_email';
+		$form->jambo_email->tabindex = 2;
+		$form->jambo_email->caption = _t( 'Email' );
+		$form->jambo_email->value = $commenter_email;
+
+		// Create the Message field
+		$form->append(
+			'text',
+			'jambo_message',
+			'null:null',
+			_t( 'Message', 'jambo' ),
+			'formcontrol_textarea'
+		)->add_validator( 'validate_required', _t( 'Your message cannot be blank.', 'jambo' ) )
+		->id = 'jambo_message';
+		$form->jambo_message->tabindex = 4;
+
+		// Create the Submit button
+		$form->append( 'submit', 'jambo_submit', _t( 'Submit' ), 'formcontrol_submit' );
+		$form->jambo_submit->tabindex = 5;
+
+		// Set up form processing
+		$form->on_success( array($this, 'process_jambo') );
+		// Return the form object
+		return $form;
 	}
-	
-	// helper function to return option values
-	public static function get( $name ) {
-		return Options::get( self::OPTION_NAME . '__' . $name );
+
+	function process_jambo( $form )
+	{
+		// get the values and the stored options.
+
+		$email = array();
+		$email['sent'] =		false;
+		$email['send_to'] =	Options::get( 'jambo__send_to' );
+		$email['name'] = $form->jambo_name->value;
+		$email['email'] = $form->jambo_email->value;
+		$email['subject'] = Options::get( 'jambo__subject' );
+		$email['message'] = $form->jambo_message->value;
+		$email['success_msg'] = Options::get ( 'jambo__success_msg','Thank you contacting me. I\'ll get back to you as soon as possible.' );
+/*		// interesting stuff, this OSA business. If it's not covered by FormUI, maybe it should be.
+		$email['osa'] =            $this->handler_vars['osa'];
+		$email['osa_time'] =       $this->handler_vars['osa_time'];
+*/		
+		// Utils::mail expects an array
+		$email['headers'] = array( 'MIME-Version' => '1.0',
+			'From' => "{$email['name']} <{$email['email']}>",
+			'Content-Type' => 'text/plain; charset="utf-8"' );
+
+// 		$email = Plugins::filter( 'jambo_email', $email /* something */ );
+		
+		$email['sent'] = Utils::mail( $email['send_to'], $email['subject'], $email['message'], $email['headers'] );
+
+		return '<p class="jambo-confirmation">' .$email ['success_msg']  .'</p>';
 	}
 
 	public function set_priorities()
@@ -56,153 +119,39 @@ class Jambo extends Plugin
 			);
 	}
 	
-	/* Set up options */
-	public function filter_plugin_config( $actions, $plugin_id )
-      {
-		if ( $plugin_id == $this->plugin_id() ) {
-			$actions[]= _t('Configure');
-		}
-		return $actions;
-	}
+	public function configure()
+	{
+		$ui = new FormUI( 'jambo_config' );
 
-	public function action_plugin_ui( $plugin_id, $action )
-	{
-		if ( $plugin_id == $this->plugin_id() ) {
-			switch ( $action ) {
-				case _t('Configure') :
-					$ui = new FormUI( self::OPTION_NAME );
-					
-					// Add a text control for the address you want the email sent to
-					$send_to = $ui->append( 'text', 'send_to', 'option:jambo__send_to', _t( 'Where To Send Email: ' ) );
-					$send_to->add_validator( 'validate_required' );
-					
-					// Add a text control for the prefix to the subject field
-					$subject_prefix = $ui->append( 'text', 'subject_prefix', 'option:jambo__subject_prefix', _t( 'Subject Prefix: ' ) );
-					$subject_prefix->add_validator( 'validate_required' );
-					
-					$show_form_on_success = $ui->append( 'checkbox', 'show_form_on_success', 'option:jambo__show_form_on_success', _t( 'Show Contact Form After Sending?: ' ) );
-					
-					// Add a text control for the prefix to the success message
-					$success_msg = $ui->append( 'textarea', 'success_msg', 'option:jambo__success_msg', _t( 'Success Message: ' ) );
-					$success_msg->add_validator( 'validate_required' );
-					
-					// Add a text control for the prefix to the subject field
-					$error_msg = $ui->append( 'textarea', 'error_msg', 'option:jambo__error_msg', _t( 'Error Message: ') );
-					$error_msg->add_validator( 'validate_required' );
-					
-					$ui->append( 'submit', 'save', 'Save' );
-					$ui->out();
-					break;
-			}
-		}
-	}
-	
-	public function filter_rewrite_rules( $rules )
-	{
-		$rules[] = new RewriteRule(array(
-			'name' => 'jambo',
-			'parse_regex' => '/^jambo\/send\/(?P<jcode>[0-9a-f]+)[\/]{0,1}$/i',
-			'build_str' => 'jambo/send/{$jcode}',
-			'handler' => 'JamboHandler',
-			'action' => 'send',
-			'priority' => 2,
-			'rule_class' => RewriteRule::RULE_PLUGIN,
-			'is_active' => 1,
-			'description' => 'Rewrite for Jambo Contact Form Plugin submittion.'
-		));
-		return $rules;
-	}
+		// Add a text control for the address you want the email sent to
+		$send_to = $ui->append( 'text', 'send_to', 'option:jambo__send_to', _t( 'Where To Send Email: ' ) );
+		$send_to->add_validator( 'validate_required' );
 
-	public function filter_rewrite_args( $args, $rulename )
-	{
-		switch( $rulename ) {
-			case 'jambo':
-				$args['jcode']= $this->get_code();
-				setcookie( 'habari_rc[' . $args['jcode'] . ']', md5( Options::get('GUID') . 'other salt' ), time()+60*60*2, '/' );
-				break;
-		}
-		return $args;
-	}
-	
-	// this allows theme authors/users to create a customized jambo.form template
-	// for Jambo to output it's form. we should have a default smarty template too.
-	public function filter_available_templates( $templates, $class ) {
-		if ( !in_array( 'jambo.form', $templates ) ) {
-			switch ( strtolower($class) ) {
-				case 'rawphpengine':
-					$templates = array_merge( $templates, array('jambo.form') );
-					break;
-				case 'hiengine':
-					$templates = array_merge( $templates, array('jambo.form') );
-					break;
-			}
-		}
-		return $templates;
-	}
-	
-	public function filter_include_template_file( $template_path, $template_name, $class )
-	{
-		if ( $template_name == 'jambo.form' ) {
-			if ( ! file_exists( $template_path ) ) {
-				switch ( strtolower($class) ) {
-					case 'rawphpengine':
-						$template_path = dirname( $this->get_file() ) . '/templates/jambo.form.php';
-						break;
-					case 'smartyengine':
-						$template_path = dirname( $this->get_file() ) . '/templates/jambo.form.tpl';
-						break;
-					case 'hiengine':
-						$template_path = dirname( $this->get_file() ) . '/templates/jambo.form.hi';
-						break;
-				}
-			}
-		}
-		return $template_path;
-	}
-	
-	// here we store the current theme object for use later
-	// saves us from creating a new theme object and using more resources.
-	public function action_add_template_vars( $theme, $handler_vars )
-	{
-		$this->theme = $theme;
+		// Add a text control for the prefix to the subject field
+		$subject_prefix = $ui->append( 'text', 'subject', 'option:jambo__subject', _t( 'Subject: ' ) );
+		$subject_prefix->add_validator( 'validate_required' );
+		
+		// Add a text control for the prefix to the success message
+		$success_msg = $ui->append( 'textarea', 'success_msg', 'option:jambo__success_msg', _t( 'Success Message: ' ) );
+		
+		$ui->append( 'submit', 'save', 'Save' );
+		return $ui;
 	}
 	
 	public function filter_post_content_out( $content )
 	{
-		$content = str_ireplace( array('<!-- jambo -->', '<!-- contactform -->'), $this->get_form(), $content );
+		$content = str_ireplace( array('<!-- jambo -->', '<!-- contactform -->'), $this->get_jambo_form()->get(), $content );
 		return $content;
 	}
 	
 	public function filter_jambo_email( $email, $handlervars )
 	{
-		if ( !$this->verify_code($handlervars['jcode']) ) {
-			ob_end_clean();
-			header('HTTP/1.1 403 Forbidden');
-			die(_t('<h1>The selected action is forbidden.</h1><p>Please enable cookies in your browser.</p>'));
-		}
 		if ( ! $this->verify_OSA( $handlervars['osa'], $handlervars['osa_time'] ) ) {
 			ob_end_clean();
 			header('HTTP/1.1 403 Forbidden');
 			die(_t('<h1>The selected action is forbidden.</h1><p>You are submitting the form too fast and look like a spam bot.</p>'));
 		}
 		
-		if ( empty( $email['name'] ) ) {
-			$email['valid']= false;
-			$email['errors']['name']= _t( '<em>Your Name</em> is a <strong>required field</strong>.' );
-		}
-		if ( empty( $email['email'] ) ) {
-			$email['valid']= false;
-			$email['errors']['email']= _t( '<em>Your Email</em> is a <strong>required field</strong>.' );
-		}
-		// validate email addy as per RFC2822 and RFC2821 with a little exception (see: http://www.regular-expressions.info/email.html)
-		elseif( !preg_match("@^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*\@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$@i", $email['email'] ) ) {
-			$email['valid']= false;
-			$email['errors']['email']= _t( '<em>Your Email</em> must be a <strong>valid email address.</strong>' );
-		}
-		if ( empty( $email['message'] ) ) {
-			$email['valid']= false;
-			$email['errors']['message']= _t( '<em>Your Remarks</em> is a <strong>required field</strong>.' );
-		}
 		if( $email['valid'] !== false ) {
 			$comment = new Comment( array(
 				'name' => $email['name'],
@@ -226,31 +175,6 @@ class Jambo extends Plugin
 		return $email;
 	}
 	
-	/**
-	 * Get a 10-digit hex code that identifies the user submitting the feedback
-	 * @param The IP address of the commenter
-	 * @return A 10-digit hex code
-	 **/	 	 	 	 
-	private function get_code( $ip = '' )
-	{
-		if( $ip == '' ) {
-			$ip = ip2long($_SERVER['REMOTE_ADDR']);
-		}
-		$code = substr(md5( Options::get('GUID') . 'more salt' . $ip ), 0, 10);
-		$code = Plugins::filter('jambo_code', $code, $ip);
-		return $code;
-	}
-
-	/**
-	 * Verify a 10-digit hex code that identifies the user submitting the feedback
-	 * @param The IP address of the commenter
-	 * @return True if the code is valid, false if not
-	 **/
-	private function verify_code( $suspect_code, $ip = '' )
-	{
-		return ( $suspect_code == $this->get_code( $ip ) );
-	}
-
 	private function get_OSA( $time ) {
 		$osa = 'osa_' . substr( md5( $time . Options::get( 'GUID' ) . self::VERSION ), 0, 10 );
 		$osa = Plugins::filter('jambo_OSA', $osa, $time);
@@ -278,60 +202,6 @@ class Jambo extends Plugin
 		return "<input type=\"hidden\" name=\"osa\" value=\"$osa\" />\n<input type=\"hidden\" name=\"osa_time\" value=\"$time\" />\n";
 	}
 
-	/**
-	 * 
-	 */
-	public static function input( $type, $name, $label, $vars = array() )
-	{
-		$style = ( array_key_exists( 'errors', $vars ) && array_key_exists( $name, $vars['errors'] ) ) ? 'class="input-warning"' : '';
-		$value = array_key_exists( $name, $vars ) ? $vars[$name] : '';
-		
-		switch ( $type ) {
-			default:
-			case 'text':
-				return '<input type="text" size="40" maxlength="50" name="' . $name . '" value="' . $value . '" ' . $style . ' />';
-				break;
-			case 'textarea':
-				return '<textarea ' . $style . ' rows="8" cols="30" name="' . $name . '">' . $value . '</textarea>';
-				break;
-		}
-	}
-
-	private function get_form()
-	{
-		if ( $this->theme instanceof Theme && $this->theme->template_exists( 'jambo.form' ) ) {
-			$vars = array_merge( User::commenter(), Session::get_set( 'jambo_email' ) );
-			
-			$this->theme->jambo = new stdClass;
-			$jambo = $this->theme->jambo;
-			
-			$jambo->form_action = URL::get('jambo');
-			$jambo->success_msg = self::get( 'success_msg' );
-			$jambo->error_msg = self::get('error_msg');
-			$jambo->show_form = true;
-			$jambo->success = false;
-			$jambo->error = false;
-
-			if ( array_key_exists( 'valid', $vars ) && $vars['valid'] ) {
-				$jambo->success = true;
-				$jambo->show_form = self::get( 'show_form_on_success' );
-			}
-			
-			if ( array_key_exists( 'errors', $vars ) ) {
-				$jambo->error = true;
-				$jambo->errors = $vars['errors'];
-			}
-
-			$jambo->name = $this->input( 'text', 'name', 'Your Name: (Required)', $vars );
-			$jambo->email = $this->input( 'text', 'email', 'Your Email: (Required)', $vars );
-			$jambo->subject = $this->input( 'text', 'subject', 'Subject: ', $vars );
-			$jambo->message = $this->input( 'textarea', 'message', 'Your Remarks: (Required)', $vars );
-			$jambo->osa = $this->OSA( $vars );
-			
-			return $this->theme->fetch( 'jambo.form' );
-		}
-		return null;
-	}
 }
 
 ?>
